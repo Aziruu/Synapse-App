869af5f0aa1a8bd8f100eb824ced7f8d4d303d9d
Aziruu <mnazzrilibrahim@gmail.com>
2026-01-01 16:33:01 +0700

    Fix Auth Frontend

--- synapse-be/app/Http/Controllers/Api/AuthController.php
+++ synapse-be/app/Http/Controllers/Api/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
             'password' => 'required',
         ]);
 
-        $user = User::where('email', $request->email)->first();
+        $user = User::with('classes')->where('email', $request->email)->first();
 
         if (! $user || ! Hash::check($request->password, $user->password)) {
             return response()->json([
